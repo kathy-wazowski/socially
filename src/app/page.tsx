3201d50ccf158d24ac127d507af667b53c61c623
Kathy <kathy.wazowski@gmail.com>
2025-02-08 17:31:07 +0800

    Add: creating CreatePost

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,20 @@
+import { currentUser } from "@clerk/nextjs/server";
+import CreatePost from "@/components/CreatePost";
 
 
 export default async function Home() {
+  const user = await currentUser()
   return (
-    <>
-      <div>Home Content</div>
-    </>
+    <div className="grid grid-cols-1 lg:grid-cols-10 gap-6">
+      <div className="lg:col-span-6">
+        {
+          user ? <CreatePost /> : null
+        }
+      </div>
+
+      <div className="hidden lg:block lg:col-span-4 sticky top-20">
+        WhoToFollow
+      </div>
+    </div>
   );
-}
+} 
